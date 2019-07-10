2acee40b9c0a277c809b590aa7b9d4707d0c2c87
hugomflavio <40389021+hugomflavio@users.noreply.github.com>
2019-07-10 07:56:54 +0200

    Completed all_random, placed contincency stops on the remaining

--- R/bed_functions.R
+++ R/bed_functions.R
@@ -3,6 +3,7 @@
 #' Function inspired by this comment in StackOverflow: https://stackoverflow.com/questions/49149839/simulate-random-positions-from-a-list-of-intervals
 #'
 #' @param n number of baits to generate
+#' @param size The size of each bait
 #' @param lengths A file containing the chromosomes' length
 #' @param exclusions A file containing regions to exclude
 #' @param regions A file containing regions to target
@@ -13,12 +14,19 @@
 #' 
 #' @export
 #' 
-main_function <- function(n, lengths, exclusions = NULL, regions = NULL, targets = NULL, seed = NULL){
+main_function <- function(n, size, lengths, exclusions = NULL, regions = NULL, targets = NULL, seed = NULL){
 	# Initial checks
 	if(!is.numeric(n))
 		stop("'n' must be numeric.\n")
 	if(length(n) != 1)
 		stop("Length of 'n' must be 1.\n")
+	if(!is.numeric(size))
+		stop("'size' must be numeric.\n")
+	if(length(size) != 1)
+		stop("Length of 'size' must be 1.\n")
+
+	# Reduce size to include the first bp
+	size <- size - 1
 
 	# Import data
 	lengths <- load_lengths(file = lengths)
@@ -46,17 +54,23 @@ main_function <- function(n, lengths, exclusions = NULL, regions = NULL, targets
 		if (!is.null(targets)) 
 			trimmed_targets <- subsample(input = targets, link = lengths[i, 1])
 
-		if(is.null(c(trimmed_exclusions, trimmed_regions, trimmed_targets))))
-			recipient[[i]] <- all_random(lengths = lengths[i, ], n = n, seed = seed)
+		if(is.null(c(trimmed_exclusions, trimmed_regions, trimmed_targets)))
+			recipient[[i]] <- all_random(length = lengths[i, ], n = n, size = size, seed = seed)
 
-		if(!is.null(c(trimmed_regions, trimmed_targets)) & is.null(trimmed.exclusions))
-			recipient[[i]] <- all_targetted(lengths = lengths[i, ], n = n, seed = seed, regions = trimmed_regions, targets = trimmed_targets)
+		if(!is.null(c(trimmed_regions, trimmed_targets)) & is.null(trimmed.exclusions)) {
+			recipient[[i]] <- all_targetted(length = lengths[i, ], n = n, size = size, seed = seed, 
+				regions = trimmed_regions, targets = trimmed_targets)
+		}
 
-		if(is.null(c(trimmed_regions, trimmed_targets)) & !is.null(trimmed_exclusions))
-			recipient[[i]] <- trimmed_random(lengths = lengths[i, ], n = n, seed = seed, exclusions = trimmed_exclusions)
+		if(is.null(c(trimmed_regions, trimmed_targets)) & !is.null(trimmed_exclusions)) {
+			recipient[[i]] <- trimmed_random(length = lengths[i, ], n = n, size = size, 
+				seed = seed, exclusions = trimmed_exclusions)
+		}
 
-		if(!is.null(c(trimmed_regions, trimmed_targets)) & !is.null(trimmed.exclusions))
-			recipient[[i]] <- trimmed_targetted(lengths = lengths[i, ], n = n, seed = seed, regions = trimmed_regions, targets = trimmed_targets, exclusions = trimmed_exclusions)
+		if(!is.null(c(trimmed_regions, trimmed_targets)) & !is.null(trimmed.exclusions)) {
+			recipient[[i]] <- trimmed_targetted(length = lengths[i, ], n = n, size = size, seed = seed,
+			  regions = trimmed_regions, targets = trimmed_targets, exclusions = trimmed_exclusions)
+		}
 
 		names(recipient)[length(recipient)] <- lengths[i, 1]
 	}
@@ -95,19 +109,64 @@ subsample <- function(input, link) {
 	return(output)
 }
 
-# Extract n number of baits from the whole chromosome length
-all_random <- function(lengths, n, size, seed = NULL) {
-	
+#' Extract n number of baits randomly from the whole chromosome length
+#' 
+#' @param length The length of the chromosome being analysed
+#' @param n number of baits to generate
+#' @param size The size of each bait
+#' @param seed A number to fix the randomization process, for reproducibility
+#' 
+#' @return a dataframe with starting and ending positions
+#' 
+#' @keywords internal
+#' 
+all_random <- function(length, n, size, seed = NULL) {
+	if (!is.null(seed))
+		set.seed(seed)
+	recipient <- matrix(ncol = 2, nrow = n)
+	recipient[, 1] <- sample(seq_len(length - size), size = n, replace = TRUE)
+	recipient[, 2] <- recipient[, 1] + size
+	colnames(recipient) <- c("Start", "Stop")
+	return(as.data.frame(recipient))
 }
 
-all_targetted <- function(lengths, n, , size, seed = NULL, regions = NULL, targets = NULL)) {
-	
+#' Extract n number of baits from the whole chromosome length, targeting specific areas
+#' 
+#' @inheritParams all_random
+#' @param regions A subset of the regions dataframe for the target chromosome
+#' @param targets A subset of the targets dataframe for the target chromosome
+#' 
+#' @return a dataframe with starting and ending positions
+#' 
+#' @keywords internal
+#' 
+all_targetted <- function(length, n, size, seed = NULL, regions = NULL, targets = NULL) {
+	stop("Triggered all_targetted\n")
 }
 
-trimmed_random <- function(lengths, n, size, seed = NULL, exclusions) {
-	
+#' Extract n number of baits randomly from parts of the chromosome length
+#' 
+#' @inheritParams all_random
+#' @param exclusions A subset of the exclusions dataframe for the target chromosome
+#' 
+#' @return a dataframe with starting and ending positions
+#' 
+#' @keywords internal
+#' 
+trimmed_random <- function(length, n, size, seed = NULL, exclusions) {
+	stop("Triggered trimmed_random\n")	
 }
 
-trimmed_targetted <- function(lengths, n, size, seed = NULL, regions = NULL, targets = NULL, exclusions) {
-
+#' Extract n number of baits from parts of the chromosome length, targeting specific areas
+#' 
+#' @inheritParams all_random
+#' @inheritParams all_targetted
+#' @inheritParams trimmed_random
+#' 
+#' @return a dataframe with starting and ending positions
+#' 
+#' @keywords internal
+#' 
+trimmed_targetted <- function(length, n, size, seed = NULL, regions = NULL, targets = NULL, exclusions) {
+	stop("Triggered trimmed_targetted\n")
 }

d3ebd9fc503b1dabf7d0f127c54bea0fb6847772
Belen Jimenez-Mena <belenjimenez85@gmail.com>
2019-07-22 10:28:27 +0200

    Include path to Python script

--- R/zzz.R
+++ R/zzz.R
@@ -4,4 +4,5 @@
 
 .onLoad <- function(libname, pkgname) {
   SeqIO <<- reticulate::import("SeqIO", delay_load = TRUE)
+  reticulate::source_python(paste0(system.file(package = "baits4pop"), "/baits4pop_extract_sequence_genome2.py"))
 }

59e678cec0f9661b7cf5255a150559b106d6840d
hugomflavio <40389021+hugomflavio@users.noreply.github.com>
2019-07-22 11:09:12 +0200

    Create python_handlers.R

---
+++ R/python_handlers.R
@@ -0,0 +1,15 @@
+#' check if Phython is installed before attempting to run python code
+#' 
+#' @keywords internal
+#' 
+#' @return logical
+#' 
+check_python <- function () {
+  trigger <- try(reticulate::py_config(), silent = TRUE)
+  if (inherits(trigger, "try-error")) {
+  	cat("Error: Python modules could not be loaded. Please install python before using baits4pop.\n")
+  	return(FALSE)
+  } else {
+ 	  return(TRUE)
+  }
+}

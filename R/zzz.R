4c7645ff5dfe18680a34281dc35c4ae7016fe020
hugomflavio <40389021+hugomflavio@users.noreply.github.com>
2019-07-22 11:39:45 +0200

    change python check

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,10 @@
 .onAttach <- function(libname, pkgname){
-  if (reticulate::py_available()) {
+  if (Sys.which("python") == "") {
+  	packageStartupMessage("Error: Could not find Python modules. Please install python before using baits4pop.")
+  } else {
   	reticulate::source_python(paste0(system.file(package = "baits4pop"), "/baits4pop_extract_sequence_genome2.py"))
  	  packageStartupMessage("Python modules successfully loaded!")
  	  cat("test")
-  } else {
-  	packageStartupMessage("Error: Python modules could not be loaded. Please install python before using baits4pop.")
   }
 }
 

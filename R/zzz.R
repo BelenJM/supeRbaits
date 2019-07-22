678426065d2b3e83d5c1ebedb4e3942fb47a7ad3
hugomflavio <40389021+hugomflavio@users.noreply.github.com>
2019-07-22 11:32:46 +0200

    onAttach test

--- R/zzz.R
+++ R/zzz.R
@@ -1,15 +1,13 @@
 .onAttach <- function(libname, pkgname){
-  trigger <- try(reticulate::py_config(), silent = TRUE)
-  if (inherits(trigger, "try-error")) {
-  	packageStartupMessage("Error: Python modules could not be loaded. Please install python before using baits4pop.")
-  } else {
+  if (reticulate::py_available()) {
+  	reticulate::source_python(paste0(system.file(package = "baits4pop"), "/baits4pop_extract_sequence_genome2.py"))
  	  packageStartupMessage("Python modules successfully loaded!")
+ 	  cat("test")
+  } else {
+  	packageStartupMessage("Error: Python modules could not be loaded. Please install python before using baits4pop.")
   }
 }
 
 .onLoad <- function(libname, pkgname) {
   SeqIO <<- reticulate::import("SeqIO", delay_load = TRUE)
-  trigger <- try(reticulate::py_config(), silent = TRUE)
-  if (!inherits(trigger, "try-error"))
-  	reticulate::source_python(paste0(system.file(package = "baits4pop"), "/baits4pop_extract_sequence_genome2.py"))
 }

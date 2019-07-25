ddb90ca4384c09b9d84cf8861084cf314f0329e1
Belen Jimenez-Mena <belenjimenez85@gmail.com>
2019-07-25 15:06:56 +0200

    modify .onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -3,13 +3,15 @@
   if (inherits(trigger, "try-error")) {
    	packageStartupMessage("Error: Could not find Python modules. Please install python before using baits4pop.")
   } else {
-  	pyth <<-  reticulate::source_python(paste0(system.file(package = "baits4pop"), "/retrieveBait.py"))
  	  packageStartupMessage("Python modules successfully loaded!")
  	  cat("test")
   }
   packageStartupMessage("Welcome to baits4pop!")
+
 }
 
-# .onLoad <- function(libname, pkgname) {
-#   SeqIO <<- reticulate::import("SeqIO", delay_load = TRUE)
-# }
+.onLoad <- function(libname, pkgname) {
+  SeqIO <<- reticulate::import("SeqIO", delay_load = TRUE)
+  pyth <<-  reticulate::source_python(paste0(system.file(package = "baits4pop"), "/retrieveBait.py"))
+
+}

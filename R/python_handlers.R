b6a08c98ee992a02c4c1e96316cd39795b9eaf07
hugomflavio <40389021+hugomflavio@users.noreply.github.com>
2020-01-22 08:20:58 +0100

    Temporary fix to force the use of the correct python version

--- R/python_handlers.R
+++ R/python_handlers.R
@@ -12,7 +12,7 @@ retrieve_baits <- function(database) {
 	path <- paste(system.file(package = "supeRbaits"), "new_retrieveBait.py", sep="/")
 	bait.file <- "temp_folder_for_supeRbaits/bait_positions.txt"
 	output.file <- "temp_folder_for_supeRbaits/bait_positions_py.txt"
-	aux.py <- system2("python", args = c(shQuote(path), shQuote(database), shQuote(bait.file)), stdout = TRUE)
+	aux.py <- system2("python3", args = c(shQuote(path), shQuote(database), shQuote(bait.file)), stdout = TRUE)
 	if(!is.null(attr(aux.py, "status")))
 		stop("Python failed to retrieve the bps.")
 	output <- data.table::fread(text = aux.py, sep = "\t")

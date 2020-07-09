dafc73aaac17f5089f3bb9043f211845c607f42d
boilnkettle <jmiguelsramos@gmail.com>
2020-07-09 23:53:38 +0100

    Remove using declaration for std namespace

--- src/dos2unix.cpp
+++ src/dos2unix.cpp
@@ -5,13 +5,11 @@
 #include <fstream>
 #include <iostream>
 
-using namespace std;
-
 // [[Rcpp::export]]
 void dos2unix(std::string fin_path, std::string fout_path) {
   // only for converting CRLF to LF
-  ifstream fin(fin_path.c_str());
-  ofstream fout(fout_path.c_str(), std::ios::binary);
+  std::ifstream fin(fin_path.c_str());
+  std::ofstream fout(fout_path.c_str(), std::ios::binary);
 
   if(!fin.good()) {
     Rcpp::stop("Error opening file '%s'. Exiting...", fin_path);
@@ -22,10 +20,12 @@ void dos2unix(std::string fin_path, std::string fout_path) {
   }
 
   char c;
-  while (fin.get(c))
-    if (c != '\r')
+  while (fin.get(c)) {
+    if (c != '\r') {
       fout << c;
-
+    }
+  }
+  
   fin.close();
   fout.close();
 }

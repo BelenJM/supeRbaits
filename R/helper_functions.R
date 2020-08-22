1b80f81611a51a83290c1ad9863d34f010c6094f
hugomflavio <40389021+hugomflavio@users.noreply.github.com>
2020-08-22 20:28:58 +0200

    remove obsulete code

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -1,18 +1,3 @@
-#' Forcefully round a number down
-#'
-#' Forces the rounding of the input to the next lower rounded value.
-#' 
-#' @param input The value to be rounded
-#' @param to The level of rounding to be applied (i.e. to=10 will round 14.8 to 10; to=1 will round i to 14)
-#' 
-#' @return The rounded value
-#' 
-#' @export
-#' 
-roundDown <- function(input, to = 10) {
-  to * (input%/%to)
-}
-
 #' Convert line endings from DOS to UNIX format
 #' 
 #' database files must have UNIX line endings for supeRbaits to work.

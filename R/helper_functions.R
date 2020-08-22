448cb6d3ff0a58100e5e9fd8ea939fd95eb0db83
hugomflavio <40389021+hugomflavio@users.noreply.github.com>
2020-08-22 13:41:19 +0200

    new convert_line_endings function

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -12,3 +12,18 @@
 roundDown <- function(input, to = 10) {
   to * (input%/%to)
 }
+
+#' Convert line endings from DOS to UNIX format
+#' 
+#' database files must have UNIX line endings for supeRbaits to work.
+#' 
+#' @param input The name of the input file
+#' @param output The name of the output file (the same as the input by default)
+#' 
+#' @return No return value. Called for side effects.
+#' 
+#' @export
+#' 
+convert_line_endings <- function(input, output = input) {
+	dos2unix(input, output)
+}

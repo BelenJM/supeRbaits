94b93e4073ccd3f00524a0cb1fdbeb61a1097a1c
hugomflavio <40389021+hugomflavio@users.noreply.github.com>
2020-09-04 11:59:15 +0200

    new function standardize_lines wraps the cpp fixLineLengths and exports it.
    (also took the chance to fix a bug in convert_line_endings)

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -15,6 +15,27 @@ convert_line_endings <- function(input, output = input) {
 		dos2unix(input, aux)
 		file.rename(aux, output)
 	} else {
-		dos2unix(input, aux)
+		dos2unix(input, output)
+	}
+}
+
+#' Ensure the database nucleotide lines have the same length
+#' 
+#' Database files must have the same number of nucleotides per line (except for the terminal lines of each sequence).
+#' 
+#' @param input The name of the input file
+#' @param output The name of the output file (the same as the input by default)
+#' 
+#' @return No return value. Called for side effects.
+#' 
+#' @export
+#' 
+standardize_lines <- function(input, output = input) {
+	if (input == output) {
+		aux <- tempfile()
+		fixLineLengths(input, aux)
+		file.rename(aux, output)
+	} else {
+		fixLineLengths(input, output)
 	}
 }
